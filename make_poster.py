3db84aa07563ddecd644e2d001072064c18bebe7
Prachi Bhattacharjee <pbhattacharjee1@crimson.ua.edu>
2025-11-10 10:46:29 -0600

    Update make_poster.py

--- make_poster.py
+++ make_poster.py
@@ -3,4 +3,5 @@ from poster_designer import build_poster
 
 if __name__ == "__main__":
     build_poster("sample_poster.json", "MyPoster.pdf")
-    print("Done. Wrote MyPoster.pdf")
+    print("Done.")
+    print("Wrote MyPoster.pdf")
